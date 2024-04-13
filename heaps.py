0f3246b9c1c9b47bef4b3e218624a5003ed484df
Faith-Ndungu <wanjirufaithndunguu#@gmail.com>
2024-04-13 19:19:38 +0300

    added file

--- heaps.py
+++ heaps.py
@@ -1,12 +1,37 @@
 import heapq
 
-num = [1,2,3,4,5]
+# num = c
 
 # max
-num = [-r for r in num]
+# num = [-r for r in num]
 
 # min
-heapq.heapify(num)
+# heapq.heapify(num)
 
-for item in num:
-    print("Item in heap:",item)
+# for item in num:
+#     print("Item in heap:",item)
+
+    # print(num)
+
+    # heapq.heappop(num)
+    # print(heap)
+
+
+# heapq.heappush(num,2)
+# print(num)
+
+
+
+import heapq
+num= [1,2,3,4,5]
+heapq.heapify(num)
+print(num)
+
+heapq.heappush(num,9)
+print(num)
+
+heapq.heappushpop(num,2)
+print(num)
+
+
+
